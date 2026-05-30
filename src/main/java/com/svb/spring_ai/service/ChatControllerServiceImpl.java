da7981885f39bb5de6a9df3f88d0220899953e72
Savya Sachi V B <savyasachivb@Savyas-MacBook-Pro.local>
2026-05-30 20:39:49 +0530

    minor changes

--- src/main/java/com/svb/spring_ai/service/ChatControllerServiceImpl.java
+++ src/main/java/com/svb/spring_ai/service/ChatControllerServiceImpl.java
@@ -1,21 +1,20 @@
 package com.svb.spring_ai.service;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
 @Service
 public class ChatControllerServiceImpl implements ChatControllerService {
 
-    public final ChatClient chatClient;
-
-    public ChatControllerServiceImpl(ChatClient.Builder chatClient) {
-        this.chatClient = chatClient.build();
-    }
+    @Autowired
+    public ChatClient.Builder chatClient;
 
     @Override
     public String chat() {
         return chatClient
+                .build()
                 .prompt()
                 .user("How is the weather like in Bangalore??")
                 .call()
@@ -25,6 +24,7 @@ public class ChatControllerServiceImpl implements ChatControllerService {
     @Override
     public Flux<String> streamChat() {
         return chatClient
+                .build()
                 .prompt()
                 .user("Tell me a joke about spring-ai")
                 .stream()
